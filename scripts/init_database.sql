015a21e8fb1b1aa2a744f1f52a86c1ad3a14dc3e
M7md-7sn <muhammed.7sn@gmail.com>
2026-06-29 02:06:47 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -28,7 +28,7 @@ USE MASTER;
 -- Drop and Recrate 'DataWarehouse' Database
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH IMMIDATE ROLLBACK;
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 	DROP DATABASE DataWarehouse;
 END
 
